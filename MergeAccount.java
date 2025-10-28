6c57f04d43e2a48d68f2a4fd7e4ffdb44788f48b
Chetan Kumar Meena <cmeena@sumologic.com>
2025-10-28 06:35:57 -0700

    Fix build warnings

--- MergeAccount.java
+++ MergeAccount.java
@@ -2,29 +2,29 @@ import java.util.*;
 
 class Solution {
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        Map<String, String> emailToName = new HashMap();
-        Map<String, ArrayList<String>> graph = new HashMap();
+        Map<String, String> emailToName = new HashMap<>();
+        Map<String, ArrayList<String>> graph = new HashMap<>();
         for (List<String> account: accounts) {
             String name = "";
             for (String email: account) {
-                if (name == "") {
+                if (Objects.equals(name, "")) {
                     name = email;
                     continue;
                 }
-                graph.computeIfAbsent(email, x-> new ArrayList<String>()).add(account.get(1));
-                graph.computeIfAbsent(account.get(1), x-> new ArrayList<String>()).add(email);
+                graph.computeIfAbsent(email, x-> new ArrayList<>()).add(account.get(1));
+                graph.computeIfAbsent(account.get(1), x-> new ArrayList<>()).add(email);
                 emailToName.put(email, name);
             }
         }
 
-        Set<String> seen = new HashSet();
-        List<List<String>> ans = new ArrayList();
+        Set<String> seen = new HashSet<>();
+        List<List<String>> ans = new ArrayList<>();
         for (String email: graph.keySet()) {
             if (!seen.contains(email)) {
                 seen.add(email);
-                Stack<String> stack = new Stack();
+                Stack<String> stack = new Stack<>();
                 stack.push(email);
-                List<String> component = new ArrayList();
+                List<String> component = new ArrayList<>();
                 while (!stack.empty()) {
                     String node = stack.pop();
                     component.add(node);

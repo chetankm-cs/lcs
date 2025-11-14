ebb2832d8e148f630e8a9011cbfc59965254c188
Chetan Kumar Meena <cmeena@sumologic.com>
2025-11-13 21:14:20 -0800

    Rewrite sudoku solver

--- P36.scala
+++ P36.scala
@@ -1,8 +1,9 @@
+@scala.annotation.nowarn
 object P36 {
-
-  def check(arr: Array[Char]) = {
-    val validNums = arr.filter(x => x != '.')
-    validNums.distinct.length == validNums.length
+   def check(arr: Array[Char]): Boolean = {
+    arr.toSeq.filter(_ !=  '.').groupMapReduce(x => x)(_ => 1)(_ + _).forall {
+      case (_, v) => v == 1
+    }
   }
 
   def isValidSudoku(board: Array[Array[Char]]): Boolean = {
@@ -14,16 +15,12 @@ object P36 {
         val arrs = board.map(arr => arr(i))
         check(arrs)
     }
-    val s = (for {
-      i <- 0 until 3
-      j <- 0 until 3
-    } yield {
-      val arr = (for {
-        a <- 0 until 3
-        b <- 0 until 3
-      } yield board(3 * i + a)(3 * j + b)).toArray
-      check(arr)
-    }).forall(identity)
+
+    val s = (for { i <- 0 until 3; j <- 0 until 3 } yield (i, j)).forall {
+       case (i,j) =>  
+          val square = (for { a <- 0 until 3; b <- 0 until 3 } yield board(3 * i + a)(3 * j + b)).toArray
+          check(square)
+    }
 
     h && v && s
   }
